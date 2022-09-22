536030ea56e5069d98c8a2cf55cec039d9af78b4
Sinu <sinuxavier2015@gmail.com>
2022-09-22 08:08:21 +0530

    Intermediate commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Housing-Prediction
+## Software and Tools Predictions
+
+[GitHub Account](https://github.com/sinu-02/Housing-Prediction)
+[VS Code IDE]()
---
+++ app.py
@@ -0,0 +1,27 @@
+import pickle
+
+import numpy as np
+from flask import Flask, request, url_for, render_template, jsonify
+import numpy
+import pandas
+
+app=Flask(__name__)
+regmodel=pickle.load(open('regmodel.pkl','rb'))
+scaler=pickle.load(open('scaling.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route("/predict_api",methods=['POST'])
+def predict_api():
+    data = request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1 ,-1))
+    new_data = scaler.transform(np.array(list(data.values())).reshape(1,-1))
+    output = regmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+if __name__ =="__main__":
+    app.run(debug=True)
